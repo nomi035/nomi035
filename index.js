f15d758e188b52c2e03c4b83255f2ba400e682e9
nomi035 <noamantariq@gmail.com>
2018-04-02 02:00:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-navigator.webkitGetUserMedia({ video: true, audio: false }, function (stream) {
-    
-    
-    //var simplePeer = require("simple-peer")
-    var peer = new SimplePeer({
-        initiator: location.hash === '#init',
-        trickle: false,
-        stream: stream
-      })
+navigator.getUserMedia({ video: true, audio: false }, function (err, stream) {
+  if (err) return console.error(err)
 
-      peer.on('signal', function (data) {
-        document.getElementById('yourId').value = JSON.stringify(data)
-      })
+  var peer = new SimplePeer({
+    initiator: location.hash === '#init',
+    trickle: false,
+    stream: stream
+  })
 
-      document.getElementById('connect').addEventListener('click', function () {
-        var otherId = JSON.parse(document.getElementById('otherId').value)
-        peer.signal(otherId)
-      })
+  peer.on('signal', function (data) {
+    document.getElementById('yourId').value = JSON.stringify(data)
+  })
 
-      document.getElementById('send').addEventListener('click', function () {
-        var yourMessage = document.getElementById('yourMessage').value
-        peer.send(yourMessage)
-      })
+  document.getElementById('connect').addEventListener('click', function () {
+    var otherId = JSON.parse(document.getElementById('otherId').value)
+    peer.signal(otherId)
+  })
 
-      peer.on('data', function (data) {
-        document.getElementById('messages').textContent += data + '\n'
-      })
-    
-      peer.on('stream', function (stream) {
-        var video = document.createElement('video')
-        document.body.appendChild(video)
+  document.getElementById('send').addEventListener('click', function () {
+    var yourMessage = document.getElementById('yourMessage').value
+    peer.send(yourMessage)
+  })
 
-        video.src = window.URL.createObjectURL(stream)
-        video.play()
-  })     
+  peer.on('data', function (data) {
+    document.getElementById('messages').textContent += data + '\n'
+  })
+
+  peer.on('stream', function (stream) {
+    var video = document.createElement('video')
+    document.body.appendChild(video)
+
+    video.src = window.URL.createObjectURL(stream)
+    video.play()
+  })
 })
