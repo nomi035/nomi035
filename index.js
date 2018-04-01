2f3a97b08fa4f178de4593043350d7700a250cf9
nomi035 <noamantariq@gmail.com>
2018-04-02 01:48:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-//navigator.webkitGetUserMedia({ video: true, audio: false }, function (stream) {
+navigator.webkitGetUserMedia({ video: true, audio: false }, function (stream) {
+    if (err) return console.error(err)
+    
+    //var simplePeer = require("simple-peer")
+    var peer = new SimplePeer({
+        initiator: location.hash === '#init',
+        trickle: false,
+        stream: stream
+      })
 
-//var simplePeer = require("simple-peer")
-var peer = new SimplePeer({
-    initiator: location.hash === '#init',
-    trickle: false,
-  })
+      peer.on('signal', function (data) {
+        document.getElementById('yourId').value = JSON.stringify(data)
+      })
 
-  peer.on('signal', function (data) {
-    document.getElementById('yourId').value = JSON.stringify(data)
-  })
+      document.getElementById('connect').addEventListener('click', function () {
+        var otherId = JSON.parse(document.getElementById('otherId').value)
+        peer.signal(otherId)
+      })
 
-  document.getElementById('connect').addEventListener('click', function () {
-    var otherId = JSON.parse(document.getElementById('otherId').value)
-    peer.signal(otherId)
-  })
+      document.getElementById('send').addEventListener('click', function () {
+        var yourMessage = document.getElementById('yourMessage').value
+        peer.send(yourMessage)
+      })
 
-  document.getElementById('send').addEventListener('click', function () {
-    var yourMessage = document.getElementById('yourMessage').value
-    peer.send(yourMessage)
-  })
+      peer.on('data', function (data) {
+        document.getElementById('messages').textContent += data + '\n'
+      })
+    
+      peer.on('stream', function (stream) {
+        var video = document.createElement('video')
+        document.body.appendChild(video)
 
-  peer.on('data', function (data) {
-    document.getElementById('messages').textContent += data + '\n'
-  })
+        video.src = window.URL.createObjectURL(stream)
+        video.play()
+  })     
+})
